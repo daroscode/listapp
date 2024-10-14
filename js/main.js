5e73a80e3082b6b57c62707962ae35f3b2ffb469
Daniel Araujo <danerscode@hotmail.com>
2024-10-14 16:50:40 -0300

    Desenvolvimento de cards

--- js/main.js
+++ js/main.js
@@ -6,70 +6,138 @@
 //     });
 // }
 
-var itemList = document.getElementById("data");
-
+// Variables setup
+var cardsList = document.getElementById("itemCards");
 var itemAddForm = document.getElementById("itemAddForm");
 var itemTitle = document.getElementById("title");
 var itemText = document.getElementById("text");
-var itemAddSubmit = document.querySelector(".submit");
-var formTitle = document.querySelector(".modal-title");
+var contentID = document.getElementById("contentID");
+var deleteButton = $("#deleteButton");
+var deleteCardsButton = $("#deleteCardsButton");
 var itemAddModal = $("#itemAdd");
+const jobDone = document.getElementById("jobDone");
+var jobDoneText = $("#jobDoneText");
 var btnItemAdd = $("#btnItemAdd");
-
 let getData = localStorage.getItem("listapp") ? JSON.parse(localStorage.getItem("listapp")) : [];
-
 let isEdit = false, editId;
-showItems();
+let isDebouncing = false;
 
-function showItems() {
-    document.querySelectorAll(".item-details").forEach(info => info.remove());
-    getData.forEach((element, index) => {
-        let createElement = `<tr class="item-details">
-        <td>${index + 1}</td>
-        <td>${element.Title}</td>
-        <td>
-            <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#itemRead" onclick="readItemDetail('${index}', '${element.Title}', '${element.Text}')"><i class="bi bi-eye"></i></button>
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#itemAdd" onclick="editItemDetail('${index}', '${element.Title}', '${element.Text}')"><i class="bi bi-pencil-square"></i></button>
-            <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#itemDelete" onclick="deleteItemDetail('${index}')"><i class="bi bi-trash"></i></button>
-        </td>
-        </tr>
-        `;
+// Page load checks
+$(document).ready(function(){
+    
+    deleteButton.hide();
+    
+    showCards();
 
-        itemList.innerHTML += createElement;
-    });
+    checkForItems();
+
+});
+
+// Functions
+
+function checkedJob(button, index) {
+
+    if (button.is(':checked')) {
+        getData.forEach((element, i) => {
+            if (index == i) {
+                $('.card-body').css('background-color', 'green');
+            }        
+        });
+  }
 }
 
-function readItemDetail(index, title, text) {
-    document.querySelector("#showTitle").value = title;
-    document.querySelector("#showText").value = text;
-}
+// Checks if the Localstorage has content. If it's empty it will hide the global button for Deleting All Cards
+function checkForItems() {
+    if (localStorage.getItem("listapp") === null) {
+        deleteCardsButton.hide();
+    }
 
-function editItemDetail(index, title, text) {
-    isEdit = true;
-    editId = index;
-    formTitle.innerText = "Editar item";
-    itemAddSubmit.innerText = "Atualizar";
-    itemTitle.value = title;
-    itemText.value = text;
-    showItems();
-}
+    var listAppContent = localStorage.getItem('listapp');
 
-function deleteItemDetail(index) {
-    if (confirm("Confirmar a exclusão?")) {
-        getData.splice(index, 1);
-        localStorage.setItem("listapp", JSON.stringify(getData));
-        showItems();
+    if (!listAppContent || listAppContent === '[]' || listAppContent === '{}' || $.trim(listAppContent) === "") {
+        // Hide the button if "listapp" is empty or contains invalid content
+        deleteCardsButton.hide();
     }
 }
 
+// Builds the new element with information retrieved from Localstorage and inserts into DOM
+function showCards() {
+    
+    if (localStorage.getItem("listapp") !== null) {
+        deleteCardsButton.show();
+    }
+
+    document.querySelectorAll(".cards-details").forEach(info => info.remove());
+    
+    getData.forEach((element, index) => {
+        let createCard = `
+        <div class="card cards-details" 
+            style="width: 18rem;" 
+            data-bs-toggle="modal" 
+            data-bs-target="#itemAdd" 
+            onclick="updateCard('${index}', '${element.Title}', '${element.Text}')">
+            <div class="card-body">
+                <span style="display:none" id="'${index}'">'${index}'</span>
+                <h5 class="card-title">${element.Title}</h5>
+                <p class="card-text">${element.Text}</p>
+            </div>
+        </div>
+        `;
+        cardsList.innerHTML += createCard;    
+    });
+
+}
+
+// Delete a single card based on the index retrieved from Localstorage
+function deleteCard(index) {
+    if (confirm("Confirmar a exclusão?")) {
+        if (isDebouncing) return;
+        isDebouncing = true;
+    
+        setTimeout(() => {
+            isDebouncing = false;
+        }, 500);
+        
+        getData.splice(index, 1);
+        localStorage.setItem("listapp", JSON.stringify(getData));
+        checkForItems();
+        location.reload();
+    }
+}
+
+// Updates a single card based on the index retrieved from Localstorage
+function updateCard(index, title, text) {
+    isEdit = true;
+    editId = index;
+    deleteButton.show();
+    itemTitle.value = title;
+    itemText.value = text;
+    jobDone.value = index;
+    showCards();
+
+    deleteButton.on("click", function() {
+        deleteCard(index);   
+    });
+}
+
+// Deletes all present content in Localstorage
+function deleteAllCards() {
+    if (confirm("Confirmar a exclusão de todos os cards?")) {
+        localStorage.removeItem('listapp');
+    }
+    checkForItems();
+    location.reload();
+}
+
+// Checks if New Item was clicked and changes base parameters
 btnItemAdd.on("click", function() {
     isEdit = false;
-    formTitle.innerText = "Adicionar novo item";
-    itemAddSubmit.innerText = "Enviar";
+    deleteButton.hide();
     itemTitle.value = '';
     itemText.value = '';
 });
 
+// Global control for the submit button to either save a new item or edit a previously created one
 itemAddForm.addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -86,6 +154,12 @@ itemAddForm.addEventListener('submit', function(e) {
     }
 
     localStorage.setItem("listapp", JSON.stringify(getData));
-    showItems();
+    showCards();
+    checkForItems();
     itemAddModal.modal('toggle');
+});
+
+jobDone.addEventListener("click", function() {
+    let contentIndex = $(this).val();
+    checkedJob($(this), contentIndex);
 });
--- sw.js
+++ sw.js
@@ -12,8 +12,6 @@ self.addEventListener('install', function(event){
             'img/144x144.png',
             'img/256x256.png',
             'img/512x512.png',
-            'img/arrow_up.png',
-            'img/arrow_down.png',
             'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
             'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css',
             'https://code.jquery.com/jquery-3.7.1.min.js',
